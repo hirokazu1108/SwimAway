6a514d6bf1b05ee3a8d334eba306ab1974591e54
hirokazu1108 <k.1108.hiro@gmail.com>
2024-02-14 11:42:07 +0900

    プレイヤークラスの作成

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    private Vector2 _dir;
+
+    void userInput()
+    {
+
+    }
+
+    void adjustSpeed()
+    {
+
+    }
+
+    void direction()
+    {
+
+    }
+
+}
