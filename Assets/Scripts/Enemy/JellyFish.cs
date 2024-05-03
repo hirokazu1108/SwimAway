3fca472a09839474d7ecd15b86b6bff3c0ccd8c6
hirokazu1108 <k.1108.hiro@gmail.com>
2024-05-03 22:38:31 +0900

    動く敵の修正

--- Assets/Scripts/Enemy/JellyFish.cs
+++ Assets/Scripts/Enemy/JellyFish.cs
@@ -21,63 +21,68 @@ public class JellyFish : Enemy
 
     private Rigidbody _rb = null;
     private Vector3 _startPos = Vector3.zero;   //反復移動の基準位置
-    private float _boundTimer = 99f;    //バウンド時間の測定
+    private bool _isBound = false;    //バウンド中かのフラグ
     private bool _dirTogle = false; //進行方向のトグル
     private Action moveFunc = null;
-
-
-    private GameObject _targetPointObj = null;
+    private float _rayDistance = .75f;
 
 
     private void Start()
     {
-        _targetPointObj = transform.GetChild(0).gameObject;
-
         _rb = GetComponent<Rigidbody>();
         _startPos = transform.position;
         moveFuncSet();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Move();
+        emitDetectRay();
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Hit(collision);
-    }
 
     private void moveFuncSet()
     {
-
         switch (_moveMode)
         {
             case MoveMode.XAxis:
                 moveFunc = () => {
                     var targetPos = Vector3.zero;
-                    if (_dirTogle) targetPos = new Vector3(_startPos.x + _roundTripWidthX, _startPos.y, _startPos.z);
-                    else targetPos = new Vector3(_startPos.x - _roundTripWidthX, _startPos.y, _startPos.z);
-                    _targetPointObj.transform.position = targetPos;
+                    if (_dirTogle)
+                    {
+                        targetPos = new Vector3(_startPos.x + _roundTripWidthX, _startPos.y, _startPos.z);
+                        transform.rotation = Quaternion.Euler(0,90,0);
+                    }
+                    else
+                    {
+                        targetPos = new Vector3(_startPos.x - _roundTripWidthX, _startPos.y, _startPos.z);
+                        transform.rotation = Quaternion.Euler(0, -90, 0);
+                    }
 
-                    if ((targetPos - transform.position).magnitude < .5f) _dirTogle = !_dirTogle;
 
-                    var dir = (targetPos - _rb.position).normalized;
-                    _rb.MovePosition(transform.position + dir*speed*Time.fixedDeltaTime);
+                    if ((targetPos - transform.position).magnitude < 1f) _dirTogle = !_dirTogle;
+
+                    transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
                 };
                 break;
             case MoveMode.YAxis:
                 moveFunc = () =>
                 {
                     var targetPos = Vector3.zero;
-                    if (_dirTogle) targetPos = new Vector3(_startPos.x, _startPos.y + _roundTripWidthY, _startPos.z);
-                    else targetPos = new Vector3(_startPos.x, _startPos.y - _roundTripWidthY, _startPos.z);
-                    _targetPointObj.transform.position = targetPos;
+                    if (_dirTogle)
+                    {
+                        targetPos = new Vector3(_startPos.x, _startPos.y + _roundTripWidthY, _startPos.z);
+                        transform.rotation = Quaternion.Euler(-90, 0, 0);
+                    }
+                    else
+                    {
+                        targetPos = new Vector3(_startPos.x, _startPos.y - _roundTripWidthY, _startPos.z);
+                        transform.rotation = Quaternion.Euler(90, 0, 0);
+                    }
 
-                    if ((targetPos - transform.position).magnitude < .5f) _dirTogle = !_dirTogle;
+                    if ((targetPos - transform.position).magnitude < 1f) _dirTogle = !_dirTogle;
 
-                    var dir = (targetPos - transform.position).normalized;
-                    _rb.MovePosition(transform.position + dir * speed * Time.fixedDeltaTime);
+                    transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
                 };
                 break;
         }
@@ -89,15 +94,10 @@ public class JellyFish : Enemy
         moveFuncSet();
     }
 
-    private void switchTargetPos()
-    {
-
-    }
-
     public override void Move()
     {
-        if (_boundTimer > _boundInterval) moveFunc();
-        else _boundTimer += Time.fixedDeltaTime;
+        if (_isBound) return;
+        moveFunc();
     }
 
     public override void Hit()
@@ -105,45 +105,44 @@ public class JellyFish : Enemy
 
     }
 
-    private void Hit(Collision collision)
+    // バウンド後のクール時間を待機
+    private void waitBoundInterval()
+    {
+        _isBound = true;
+        Invoke("exitBoundState", _boundInterval);
+    }
+
+    //バウンドのクール時間を終了
+    private void exitBoundState()
     {
         _rb.velocity = Vector3.zero;
-
-        // 衝突位置を取得する
-        var hitPos = collision.contacts[0].point;
-
-        // 自分から衝突位置へ向かうベクトルを求める
-        Vector3 boundVec = collision.gameObject.transform.position - hitPos;
-
-        var player = collision.gameObject.GetComponent<Player>();
-
-        var angle = Mathf.Atan2(boundVec.y, boundVec.x) * Mathf.Rad2Deg;
-
-        var forceDirection = Vector2.zero;
-
-        //敵の位置で条件分け
-        if (angle < 135 && angle < -135)    //後方
-        {
-            forceDirection = new Vector2(-1, 0);
-        }
-        else if (angle < -45)   //下方
-        {
-            forceDirection = new Vector2(0, -1);
-        }
-        else if (angle < 45)    //前方
-        {
-            forceDirection = new Vector2(1, 0);
-        }
-        else //上方
-        {
-            forceDirection = new Vector2(0, 1);
-        }
-
-        _boundTimer = 0;
-        _dirTogle = !_dirTogle; //目標地点の変更
-        if (collision.gameObject.CompareTag("Player")) player.addForce(forceDirection, _boundPower);
-        _rb.AddForce(-forceDirection * _boundPower, ForceMode.Impulse);
-
-        if (isSwitchingDirection) switchMoveMode(); //方向切り替え
+        _isBound = false;
     }
+
+    //進行方向にプレイヤーがいたら押す
+    private void emitDetectRay()
+    {
+        if (_isBound) return;
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(transform.position, transform.forward * _rayDistance, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, _rayDistance))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                var player = hit.collider.gameObject.GetComponent<Player>();
+
+                player.addForce(transform.forward, _boundPower);
+                _rb.AddForce(-transform.forward * _boundPower, ForceMode.Impulse);
+
+                waitBoundInterval();    //クールタイム処理
+                _dirTogle = !_dirTogle; //目標地点の変更
+
+                if (isSwitchingDirection) switchMoveMode(); //方向切り替え
+            }
+        }
+    }
+
 }
