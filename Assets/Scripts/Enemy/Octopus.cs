bbde64916f1281a780b5dab6359f7cbd5ffa0c30
hirokazu1108 <k.1108.hiro@gmail.com>
2024-05-26 21:11:41 +0900

    タコの追加

--- Assets/Scripts/Enemy/Octopus.cs
+++ Assets/Scripts/Enemy/Octopus.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Octopus : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _particleObject_OctopusInk;
+    [SerializeField,Tooltip("墨のエフェクト")] private ParticleSystem _octopusInkParticle;
 
+    private GameObject _tagetObject = null;
+
+    private void Update()
+    {
+        if (_tagetObject != null)
+        {
+            Vector3 vector3 = _tagetObject.transform.position - this.transform.position;    // 対象物と自分自身の座標からベクトルを算出
+            // もし上下方向の回転はしない(Baseオブジェクトが床から離れないようにする)ようにしたければ以下のようにする。
+            // vector3.y = 0f;
+
+            Quaternion quaternion = Quaternion.LookRotation(vector3);    // Quaternion(回転値)を取得
+            this.transform.rotation = quaternion;
+        }
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        _particleObject_OctopusInk.Play();
+        if (other.CompareTag("Player"))
+        {
+            _tagetObject = other.gameObject;
+            _octopusInkParticle.Play();
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _tagetObject = null;
+        }
+    }
+
 }
