69b66b58553252e8305bef436f4c6cfe4a3367e4
hirokazu1108 <k.1108.hiro@gmail.com>
2024-05-26 22:52:54 +0900

    タコの追加

--- Assets/Scripts/Enemy/Octopus.cs
+++ Assets/Scripts/Enemy/Octopus.cs
@@ -1,14 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Octopus : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _particleObject_OctopusInk;
+    [SerializeField, Header("墨のエフェクト")] private ParticleSystem _octopusInkParticle;
+    [SerializeField, Header("発見から発射までのインターバル")] private float _shootIntervalFromFind;
+    [SerializeField] private float _rotateSpeed;
 
+    private GameObject _tagetObject = null;
+    private Quaternion _originalRotation = Quaternion.identity;
+
+
+    private void Start()
+    {
+        _originalRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if(!_octopusInkParticle.isPlaying)   Look();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        _particleObject_OctopusInk.Play();
+        if (other.CompareTag("Player"))
+        {
+            _tagetObject = other.gameObject;
+            StartCoroutine(ShootInk());
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _tagetObject = null;
+        }
+    }
+
+    private void Look()
+    {
+        Quaternion rot = Quaternion.identity;
+
+        if (_tagetObject != null)   // プレイヤー発見中
+        {
+            Vector3 vector3 = _tagetObject.transform.position - this.transform.position;    // 対象物と自分自身の座標からベクトルを算出
+            rot = Quaternion.LookRotation(vector3);    // Quaternion(回転値)を取得
+        }
+        else
+        {
+            rot = _originalRotation;
+        }
+
+        rot = Quaternion.Slerp(this.transform.rotation, rot, Time.deltaTime * _rotateSpeed);
+        this.transform.rotation = rot;
+    }
+
+    private IEnumerator ShootInk()
+    {
+        yield return new WaitForSeconds(_shootIntervalFromFind);
+        if(_tagetObject != null)    _octopusInkParticle.Play();
+        yield break;
+    }
+
 }
