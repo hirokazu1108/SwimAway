20ac8ccadc3c8976f6d1a30b04842725f34a3e22
hirokazu1108 <k.1108.hiro@gmail.com>
2024-07-03 22:37:28 +0900

    タイトルの修正

--- Assets/Scripts/Title/TitleUIManager.cs
+++ Assets/Scripts/Title/TitleUIManager.cs
@@ -5,7 +5,7 @@ public class TitleUIManager : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             if (StageManager.ExistsNextStage())
             {
