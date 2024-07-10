c2c1119c74da96c5dbb0880d14f1e1cae606fcea
nakayamaharuka <c612101321@tokushima-u.ac.jp>
2024-07-10 11:10:26 +0900

    テキストフェードのみに変更

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -11,67 +11,71 @@ public class Fade : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI stageText;
     
-    private float Speed = 0.001f;
-    private float Speed2 = 0.005f;
-    private float red, green, blue, alfa, alfaText;
+    private float imageSpeed = 0.001f;
+    private float textSpeed = 0.003f;
 
-    public bool fadeOut = false;
-    public bool fadeIn = false;
+    private float[] imageColor = new float[4];
+    private float[] textColor = new float[4];
+
+    public bool imageFade = false;
+    public bool textFade = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        red = fadeImage.color.r;
-        green = fadeImage.color.g;
-        blue = fadeImage.color.b;
-        alfa = fadeImage.color.a;
+        imageColor[0] = fadeImage.color.r;
+        imageColor[1] = fadeImage.color.g;
+        imageColor[2] = fadeImage.color.b;
+        imageColor[3] = fadeImage.color.a;
 
-        alfaText = stageText.color.a;
+        textColor[0] = stageText.color.r;
+        textColor[1] = stageText.color.g;
+        textColor[2] = stageText.color.b;
+        textColor[3] = stageText.color.a;
 
-        fadeIn = true;
+        //imageFade = true;
+        textFade = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fadeIn)
+        if (imageFade)
         {
-            FadeIn();
+            ImageFade();
         }
 
-        if (fadeOut)
+        if(textFade)
         {
-            FadeOut();
+            TextFade();
         }
     }
 
-    void FadeIn()
+    private void ImageFade()
     {
-        alfa -= Speed;
-        alfaText -= Speed2;
+        imageColor[3] -= imageSpeed;
         Alpha();
-        if(alfa <= 0)
+        if(imageColor[3] <= 0)
         {
-            fadeIn = false;
+            imageFade = false;
             fadeImage.enabled = false;
         }
     }
 
-    void FadeOut()
+    private void TextFade()
     {
-        fadeImage.enabled = true;
-        alfa += Speed;
-        alfaText += Speed2;
+        textColor[3] -= textSpeed;
         Alpha();
-        if(alfa >= 1)
+        if (textColor[3] <= 0)
         {
-            fadeOut = false;
+            textFade = false;
+            stageText.enabled = false;
         }
     }
 
-    void Alpha()
+    private void Alpha()
     {
-        fadeImage.color = new Color(red, green, blue, alfa);
-        stageText.color = new Color(255, 255, 255, alfaText);
+        fadeImage.color = new Color(imageColor[0], imageColor[1], imageColor[2], imageColor[3]);
+        stageText.color = new Color(textColor[0], textColor[1], textColor[2], textColor[3]);
     }
 }
